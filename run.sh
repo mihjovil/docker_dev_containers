f0e470b03cc7cbde59021844d662b97e7f3cda35
Miguel Angel Caldas <ma.caldas331@gmail.com>
2023-03-27 07:34:41 +0200

    :bug: first line was wrong

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/sh
 # This script is used to run an existing image generated from one of the Dockerfiles and the build.sh script
 # Replace the variables with the correct names in order to use this script
 CONTAINER_NAME="CONTAINER"
