ca1dc855e7e75d014f21011fc7bd69e8ade26ac2
mihjovil <ma.caldas331@gmail.com>
2024-12-28 23:59:03 +0100

    :bug: the script was faulty as it was using . and the dockerfile name

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 # This file is used with one Dockerfile from the repo to build an docker image
 # Replace the variables in the script with the correct values in order to use this script
-DOCKERFILE_PATH="FILE"
 IMAGE_NAME="IMAGE"
-docker build . $DOCKERFILE_PATH -t $IMAGE_NAME
+docker build . -t $IMAGE_NAME
