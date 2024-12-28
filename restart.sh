867697b4f46d87c5a607098fa0f0943f50ff7d5f
mihjovil <ma.caldas331@gmail.com>
2024-12-28 23:58:05 +0100

    :fire: removed wrong file with faulty info

--- restart.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-# This script is used directly after creating the container as it will otherwise keep the CLI busy.
-# The script will stop the container, start it again (without blocking the CLI) and open an interactive
-# CLI with the container.
-# Replace the varialbles values with the correct ones in order to use this script.
-IMAGE_NAME="IMAGE"
-docker exec -it $IMAGE_NAME /bin/bash
